e8b98e578bec1123202a2f4837dae2782f30bcec
jul <julien.wist@correounivalle.edu.co>
2020-05-20 07:56:10 -0500

    chore: refactor the export

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 const { readFileSync } = require("fs");
 const { join } = require("path");
+const recursive = require('recursive-readdir-synchronous');
 
-let path = join(__dirname, "./Rutin_NMRdata_400MHz_DMSOd6_JDX/");
-
-const Rutin = {
-  path: path,
-  experiment: {
-    proton: readFileSync(
-      join(path, "Rutin_3080ug200uL_DMSOd6_qHNMR_400MHz_JDX.jdx")
-    ),
-    carbon: readFileSync(
-      join(path, "Rutin_3080ug200uL_DMSOd6_13CNMR_400MHz_JDX.jdx")
-    ),
-    cosy: readFileSync(
-      join(path, "Rutin_3080ug200uL_DMSOd6_COSY_400MHz_JDX.jdx")
-    ),
-    hsqc: readFileSync(
-      join(path, "Rutin_3080ug200uL_DMSOd6_HSQC_400MHz_JDX.jdx")
-    ),
-    hmbc: readFileSync(
-      join(path, "Rutin_3080ug200uL_DMSOd6_HMBC_400MHz_JDX.jdx")
-    ),
-  },
-};
-
+const toc = JSON.parse(readFileSync(join(__dirname, 'toc.json')));
+let jcamp = {};
+for (let file of toc){
+  let name = file.filename;
+  jcamp[name] = readFileSync(join(__dirname, file.url));
+}
 module.exports = {
-  Rutin,
+  jcamp,
 };
+
+// const { readFileSync } = require("fs");
+// const { join } = require("path");
+
+// let path = join(__dirname, "./Rutin_NMRdata_400MHz_DMSOd6_JDX/");
+
+// const Rutin = {
+//   path: path,
+//   experiment: {
+//     proton: readFileSync(
+//       join(path, "Rutin_3080ug200uL_DMSOd6_qHNMR_400MHz_JDX.jdx")
+//     ),
+//     carbon: readFileSync(
+//       join(path, "Rutin_3080ug200uL_DMSOd6_13CNMR_400MHz_JDX.jdx")
+//     ),
+//     cosy: readFileSync(
+//       join(path, "Rutin_3080ug200uL_DMSOd6_COSY_400MHz_JDX.jdx")
+//     ),
+//     hsqc: readFileSync(
+//       join(path, "Rutin_3080ug200uL_DMSOd6_HSQC_400MHz_JDX.jdx")
+//     ),
+//     hmbc: readFileSync(
+//       join(path, "Rutin_3080ug200uL_DMSOd6_HMBC_400MHz_JDX.jdx")
+//     ),
+//   },
+// };
+
+// module.exports = {
+//   Rutin,
+// };
+
---
+++ nodeScript.js
@@ -0,0 +1,28 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
+const recursive = require('recursive-readdir-synchronous');
+
+//let path = join(__dirname, "./data/Rutin_NMRdata_400MHz_DMSOd6_Jeol/");
+let path = join(__dirname, "data");
+
+let parentDir = join(__dirname);
+let files = recursive(path).map((file) =>
+  file.replace(parentDir, '.'),
+);
+
+let toc = [];
+
+for (let file of files) {
+  let parts = file.split('/');
+  toc.push({
+    filename: parts[2],
+    url: file,
+  });
+}
+
+writeFileSync(
+    join(__dirname, './toc.json'),
+    `export const toc = ${JSON.stringify(toc, undefined, 2)};`,
+  );
+  
+console.log(JSON.stringify(toc, undefined, 2));
