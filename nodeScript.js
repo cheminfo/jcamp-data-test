3ff89335e7780785af7d5dbaec089d8b702b7074
Luc Patiny <luc@patiny.com>
2022-05-08 07:07:15 +0200

    feat: add qmagnetics example

--- nodeScript.js
+++ nodeScript.js
@@ -1,28 +1,26 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { writeFileSync } = require("fs");
 const { join } = require("path");
-const recursive = require('recursive-readdir-synchronous');
+const recursive = require("recursive-readdir-synchronous");
 
 //let path = join(__dirname, "./data/Rutin_NMRdata_400MHz_DMSOd6_Jeol/");
 let path = join(__dirname, "data");
 
 let parentDir = join(__dirname);
-let files = recursive(path).map((file) =>
-  file.replace(parentDir, '.'),
-);
+let files = recursive(path).map((file) => file.replace(parentDir, "."));
 
 let toc = [];
 
 for (let file of files) {
-  let parts = file.split('/');
+  let parts = file.split("/");
   toc.push({
-    filename: parts[2],
+    filename: parts.slice(2).join(" "),
     url: file,
   });
 }
 
 writeFileSync(
-    join(__dirname, './toc.json'),
-    `${JSON.stringify(toc, undefined, 2)}`,
-  );
-  
+  join(__dirname, "./toc.json"),
+  `${JSON.stringify(toc, undefined, 2)}`
+);
+
 console.log(JSON.stringify(toc, undefined, 2));
