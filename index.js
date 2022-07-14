6f5bce4ee5feea6f5e714b4213a8f925c242bcc3
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-07-14 14:29:26 -0500

    feat!: use fileList instead toc.json

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const { readFileSync } = require("fs");
 const { join } = require("path");
 
-const toc = JSON.parse(readFileSync(join(__dirname, 'toc.json')));
-let jcamp = {};
-for (let file of toc){
-  let name = file.filename;
-  jcamp[name] = readFileSync(join(__dirname, file.url)).toString();
-}
-module.exports = {
-  jcamp,
-};
+const { fileListFromPath } = require('filelist-utils');
 
+const path = join(__dirname, 'data');
+
+module.exports = {
+  jcamp: () =>fileListFromPath(path),
+};
