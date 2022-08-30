4618072965e484047ca65ed77ffb7e7f97a42791
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-30 14:54:32 -0500

    feat!: add more specific functions

--- src/__tests__/simple.test.js
+++ src/__tests__/simple.test.js
@@ -1,9 +1,18 @@
-import { getList } from "..";
+import { getFile, getFileList, getList } from "..";
 
 describe("simple test", () => {
+  const filename = "aspirin-1h.dx";
   it("check one name", async () => {
     const list = await getList();
 
-    expect(list).toContain("aspirin-1h.dx");
+    expect(list).toContain(filename);
+  });
+  it("check getFile", async () => {
+    const file = await getFile(filename);
+    expect(file.name).toBe(filename);
+  });
+  it("check getFileList", async () => {
+    const fileList = await getFileList();
+    expect(fileList.map((f) => f.name)).toContain(filename);
   });
 });
