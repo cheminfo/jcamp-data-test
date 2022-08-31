f2a2128059bb1bf6f3bc1a2468753cf849f9d471
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-31 09:27:50 -0500

    chore: update types

--- jcamp-data-test.d.ts
+++ jcamp-data-test.d.ts
@@ -2,6 +2,8 @@ import { PartialFile } from "filelist-utils";
 
 export async function getList(): Promise<string[]>;
 
+export async function getFile(name: string): Promise<PartialFile>;
+
 export async function getFileList(name: string): Promise<PartialFile>;
 
 export async function getData(name: string): Promise<ArrayBuffer>;
