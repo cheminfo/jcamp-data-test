bbb64c5cfb9e50899b1b236cb47ea75ef8a5f1a6
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2022-09-06 14:27:26 -0500

    feat!: export more specific functions (#8)

--- jcamp-data-test.d.ts
+++ jcamp-data-test.d.ts
@@ -1,7 +1,9 @@
 import { PartialFile } from "filelist-utils";
 
-export async function getList(): Promise<string[]>;
+export function getList(): Promise<string[]>;
 
-export async function getFileList(name: string): Promise<PartialFile>;
+export function getFile(name: string): Promise<PartialFile>;
 
-export async function getData(name: string): Promise<ArrayBuffer>;
+export function getFileList(name: string): Promise<PartialFile>;
+
+export function getData(name: string): Promise<ArrayBuffer>;
