b6023450dd691dfd05cc216c39889e36e3fccc09
jobo322 <jose.bolanos@correounivalle.edu.co>
2024-02-14 08:57:50 -0500

    feat: add types in the code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { join } from 'path';
 
+import { DataXY } from 'cheminfo-types';
 import { FileCollection } from 'file-collection';
 import { convert } from 'jcampconverter';
 import { xySortX } from 'ml-spectra-processing';
 
 const path = join(__dirname, '../data/');
 
-async function getFileCollection() {
+type ConvertResult = ReturnType<typeof convert>;
+
+async function getFileCollection(): Promise<FileCollection> {
   const fileCollection = new FileCollection();
   await fileCollection.appendPath(path);
   fileCollection.files.forEach((file) => {
@@ -15,12 +18,12 @@ async function getFileCollection() {
   return fileCollection;
 }
 
-export async function getList() {
+export async function getList(): Promise<string[]> {
   const fileCollection = await getFileCollection();
   return fileCollection.files.map((f) => f.relativePath);
 }
 
-export async function getFile(relativePath) {
+export async function getFile(relativePath: string) {
   const fileCollection = await getFileCollection();
   const file = fileCollection.files.find(
     (d) => d.relativePath === relativePath,
@@ -31,13 +34,13 @@ export async function getFile(relativePath) {
   return file;
 }
 
-export async function getParsedFile(name) {
+export async function getParsedFile(name: string): Promise<ConvertResult> {
   const file = await getFile(name);
   const jcamp = await file.text();
   return convert(jcamp, { noContour: true });
 }
 
-export async function getXY(name) {
+export async function getXY(name: string): Promise<DataXY> {
   const parsed = await getParsedFile(name);
   const firstSpectrum = parsed.flatten[0]?.spectra[0]?.data;
   return xySortX(firstSpectrum);
