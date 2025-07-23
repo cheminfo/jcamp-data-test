fccd09719c1a590498e73b36b42f4bce14c9bb92
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-07-23 12:05:01 +0200

    refactor!: use `data-test-api` instead `file-collection` (#26)

--- src/__tests__/simple.test.ts
+++ src/__tests__/simple.test.ts
@@ -1,26 +1,39 @@
 import { convert } from 'jcampconverter';
 import { describe, expect, it } from 'vitest';
 
-import { getFile, getList, getParsedFile, getXY } from '../index.js';
+import { jcampFiles } from '../index.js';
 
 describe('simple test', () => {
   const relativePath = 'nmr/aspirin/1h.dx';
   it('check one name', async () => {
-    const list = await getList();
-    expect(list).toContain(relativePath);
+    const list = await jcampFiles.files();
+    expect(list.map((f) => f.relativePath)).toContain(relativePath);
   });
   it('check getFile', async () => {
-    const file = await getFile(relativePath);
-    expect(file.relativePath).toBe(relativePath);
+    await expect(jcampFiles.getFile(relativePath)).resolves.toMatchObject({
+      relativePath,
+    });
   });
   it('check getParsedFile', async () => {
-    const parsed = await getParsedFile(relativePath, convert);
+    const parsed = await jcampFiles.getParsedData(relativePath, convert);
     expect(parsed.flatten).toHaveLength(1);
   });
   it('check getXY', async () => {
-    const dataXY = await getXY(relativePath, convert);
+    const dataXY = await jcampFiles.getXYData(relativePath, convert);
     expect(dataXY.x[0]).toBeLessThan(dataXY.x[1]);
     expect(dataXY.x).toHaveLength(32768);
     expect(dataXY.y).toHaveLength(32768);
+  });
+
+  it('should findParsedData', async () => {
+    await expect(
+      jcampFiles.findParsedData('ethylbenzene_ei.jdx', convert),
+    ).resolves.toBeDefined();
+  });
+
+  it('should findXYData', async () => {
+    await expect(
+      jcampFiles.findXYData('ethylbenzene_ei.jdx', convert),
+    ).resolves.toBeDefined();
   });
 });
